33585822f5b382bdab81f020492b717f96ba8687
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-16 15:25:08 -0700

    feat: ship tests

---
+++ spec/ship_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require './lib/ship'
+
+RSpec.describe Ship do
+  let(:cruiser) {cruiser = Ship.new("Cruiser", 3)}
+  
+  it 'exists' do
+    expect(cruiser).to be_a Ship
+  end
+
+  xit 'the ship has a name' do
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  xit 'the ship has a length' do
+    expect(cruiser.length).to eq(3)
+  end
+
+  xit 'the ship has health' do
+    expect(cruiser.health).to eq(3)
+  end
+
+  xit 'the ship is not sunk by default' do
+    expect(cruiser.sunk?).to be false
+  end
+
+  xit 'the ship can can lose health' do
+    cruiser.hit
+
+    expect(cruiser.health).to eq(2)
+  end
+
+  xit 'the can be sunk' do
+    3.times do
+      cruiser.hit
+    end
+
+    expect(cruiser.sunk?).to be true
+  end
+end
