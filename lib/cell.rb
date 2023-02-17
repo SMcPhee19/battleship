6ba1898780e859ab0ae4faeece583b12bfa3f944
Nick Sacco <nicksacco10@gmail.com>
2023-02-16 17:04:06 -0800

    Defined fired upon methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,19 +1,32 @@
 class Cell
   attr_reader :coordinate,
-              :ship,
-              :cell
-  def initialize(coordinate, ship = nil, cell = true)
+              :ship
+
+  def initialize(coordinate, ship = nil)
     @coordinate = coordinate
     @ship = ship
-    @cell = cell 
+    @fired_upon = false
   end
 
   def empty?
-    return true if @ship.nil?
-    false
+    @ship == nil
   end
 
   def place_ship(boat_name)
     @ship = boat_name
   end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @ship.nil?
+      @fired_upon = true
+    else
+      @ship.hit
+      @fired_upon = true
+    end
+  end
 end
+
