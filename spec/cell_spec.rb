1d0f8e049dc5e38fbb725ddf6e3c81ce13bf891f
Nick Sacco <nicksacco10@gmail.com>
2023-02-16 18:03:24 -0800

    Test: Finish iteration 1

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -63,6 +63,54 @@ let(:cell) {cell = Cell.new("B4")}
 
       expect(cell.fired_upon?).to be true
     end
+  end
+
+  describe 'Cell can render' do
+    let(:cruiser) {cruiser = Ship.new("Cruiser", 3)}
+    let(:cell_1) {cell_1 = Cell.new("B4")}
+    let(:cell_2) {cell_2 = Cell.new("C3")}
+
+    it 'can render has default' do
+      expect(cell_1.render).to eq(".")
+    end
+
+    it 'renders differently if it is a miss' do
+      cell_1.fire_upon
+
+      expect(cell_1.render).to eq("M")
+    end
+
+    it 'can place and render' do
+      cell_2.place_ship(cruiser)
+
+      expect(cell_2.render).to eq(".")
+      expect(cell_2.render(true)).to eq("S")
+    end
+
+    it 'ship can be hit, render correctly and does not sink' do
+      cell_2.place_ship(cruiser)
+      cell_2.fire_upon
+      expect(cell_2.render).to eq("H")
+      expect(cruiser.sunk?).to be false
+    end
+
+    it 'can be sunk and render correctly' do
+      cell_2.place_ship(cruiser)
+      3.times do
+        cell_2.fire_upon
+      end
+
+      expect(cruiser.sunk?).to be true
+      expect(cell_2.render).to eq("X")
+    end
+
+    it 'can determine if a ship is present' do
+      cell_2.place_ship(cruiser)
+      expect(cell_2.ship_present?).to be true
+    end
+    
+
+    
 
   end
 
