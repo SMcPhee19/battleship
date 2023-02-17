ea6da9a31d969bf7d37bbcc43eec9fb25cad6e41
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-17 14:40:56 -0700

    feat: starts board class tests

---
+++ spec/board_spec.rb
@@ -0,0 +1,53 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+RSpec.describe Board do
+  let(:board) {board = Board.new}
+  let(:cruiser) {cruiser = Ship.new("Cruiser", 3)}
+  let(:submarine) {submarine = Ship.new("Submarine", 2)}
+
+  describe '#the board' do
+    it 'the board exists' do
+      expect(board).to be_a Board
+    end
+
+    xit 'the board has a hash of cell objects' do
+      expect(board.cells).to eq({cells})
+    end
+  end
+
+  describe '#valid cooridnates' do
+    xit 'the board can identify and validate a cooordinate' do
+      expect(board.valid_coordinate?("A1")).to be true
+      expect(board.valid_coordinate?("E1")).to be false
+    end
+  end
+
+  describe '#valid placements' do
+    xit 'it can check if the ships placement is valid' do
+      #we might have to place the ship
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true 
+    end
+
+    xit 'it can check if the coordiante are consecutive in the correct direction' do
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+      expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+    end
+
+    xit 'it can check for and reject diagonal coordiates' do
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+    end
+
+  end
+
+
+
+end
