f1ef8656f42096fbb4e1712939a86374f7d20247
Nick Sacco <nicksacco10@gmail.com>
2023-02-17 16:57:54 -0800

    Adjust test order

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,22 +31,25 @@ RSpec.describe Board do
       #we might have to place the ship
       expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
       expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
-      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
-      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true 
+      
     end
 
-    xit 'it can check if the coordiante are consecutive in the correct direction' do
+    it 'it can check if the coordiante are consecutive in the correct direction' do
       expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
       expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
     end
 
-    xit 'it can check for and reject diagonal coordiates' do
+    it 'it can check for and reject diagonal coordiates' do
       expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
       expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
     end
 
+    it 'can confirm placement is correct after passing previous checks' do
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true 
+    end
   end
 
 
