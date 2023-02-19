e311a4b734b4daa76babe9a3487c5d404653d318
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-19 16:22:26 -0700

    refactor: start_up method

--- lib/game.rb
+++ lib/game.rb
@@ -17,18 +17,15 @@ class Game
 
   def start_up
     puts "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
-    user_input = gets.chomp
-    if user_input == "p"
+    input = gets.chomp
+    if input == "p"
       game_setup
-      computer_setup
-      player_setup
+      computer_ship_placement
+      player_ship_placement
       play_game
     elsif
-      user_input == "q"
+      input == "q"
       puts "That's dissapointing!"
-    else
-      puts "Read the directions, p or q"
-      start_up
     end
   end
 
@@ -41,15 +38,28 @@ class Game
     @player_cruiser = Ship.new('Cruiser', 3)
   end
 
+  
   def computer_ship_placement
     computer_place_cruiser = @computer_board.valid_cruiser_placement.sample
     @computer_board.place(@computer_cruiser, computer_place_cruiser)
     computer_place_sub = @computer_board.valid_submarine_placement.sample
     @computer_board.place(@computer_sub, computer_place_sub)
     while @computer_board.valid_placement?(@computer_sub, computer_place_sub) == false
+      break #We'll need to refactor some how find a way to get rid of the break
     end
     @computer_board.place(@computer_sub, computer_place_sub)
   end
+  
+  def player_ship_placement
+    puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the\nSubmarine is two units long."
+    puts @player_board.render(true)
+    
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    player_cruiser_cells = []
+    puts "#{player_cruiser_cells << gets.chomp.capitalize.join(" ")}"
+  
 
+  end
+  
   
 end
