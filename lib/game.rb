c5c87a25fa04eb540291f3271ed97d6479ccab03
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-21 16:29:18 -0700

    Change computer board back to normal after bug fixing

--- lib/game.rb
+++ lib/game.rb
@@ -166,7 +166,7 @@ class Game
 
   def show_board
     puts "=============COMPUTER BOARD============="
-    puts @computer_board.render(true)
+    puts @computer_board.render
     puts "==============PLAYER BOARD=============="
     puts @player_board.render(true)
   end
